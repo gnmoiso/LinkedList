5f051b16dd828d5d63243d21fdd797914c600760
gnmoiso <moisoyt@gmail.com>
2025-10-07 12:30:40 -0500

    Basic Example for Simple List

---
+++ LinkedList/LinkedList.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using LinkedList.Core;
+
+var list = new SimplyLinkedList<string>();
+var opc = string.Empty;
+do
+{
+    opc = Menu();
+    switch (opc)
+    {
+        case "0":
+            Console.WriteLine("Exiting...");
+            break;
+        case "1":
+            Console.Write("Enter value to insert at beginning: ");
+            list.InsrtAtBeginning(Console.ReadLine()!);
+            break;
+        case "2":
+            Console.Write("Enter value to insert at end: ");
+            list.InsrtAtEnd(Console.ReadLine()!);
+            break;
+        case "3":
+            list.PrintList();
+            break;
+        default:
+            Console.WriteLine("Invalid option");
+            break;
+    }
+    } while (opc != "0");
+
+string Menu()
+{
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("1. Insert at Beginning");
+    Console.WriteLine("2. Insert at End");
+    Console.WriteLine("3. Print List");
+    Console.WriteLine("0. Exit");
+    Console.Write("Choose an option: ");
+    return Console.ReadLine()!;
+}
---
+++ LinkedList/LinkedList.Core/SimplyLinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList.Core;
+
+public class SimplyLinkedList<T>
+{
+    private SimpleNode<T>? head;
+
+    public SimplyLinkedList()
+    {
+        head = null;
+    }
+
+    public void InsrtAtBeginning(T data)
+    {
+        var newNode = new SimpleNode<T>(data);
+        newNode.Next = head;
+        head = newNode;
+    }
+
+    public void InsrtAtEnd(T data)
+    {
+        var newNode = new SimpleNode<T>(data);
+        if (head == null)
+        {
+            head = newNode;
+            return;
+        }
+        var current = head;
+        while (current.Next != null)
+        {
+            current = current.Next;
+        }
+        current.Next = newNode;
+    }
+
+    public void PrintList()
+    {
+        var current = head;
+        while (current != null)
+        {
+            Console.Write($"{current.Data} -> ");
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+}
