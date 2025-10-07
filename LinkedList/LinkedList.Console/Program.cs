0cf7f4b2794bf70fb0bf9a1d6187437ede84ce7f
gnmoiso <moisoyt@gmail.com>
2025-10-07 13:17:28 -0500

    Completed Total Methods Implementation

--- LinkedList/LinkedList.Console/Program.cs
+++ LinkedList/LinkedList.Console/Program.cs
@@ -10,31 +10,77 @@ do
         case "0":
             Console.WriteLine("Exiting...");
             break;
+
         case "1":
             Console.Write("Enter value to insert at beginning: ");
             list.InsrtAtBeginning(Console.ReadLine()!);
             break;
+
         case "2":
             Console.Write("Enter value to insert at end: ");
             list.InsrtAtEnd(Console.ReadLine()!);
             break;
+
         case "3":
             list.PrintList();
             break;
+
+        case "4":
+            Console.WriteLine("Enter value to search: ");
+            var read = Console.ReadLine();
+            bool wasFound = list.Contains(read!);
+            if (wasFound == true)
+            {
+                Console.WriteLine($"The Value {read} is found");
+            }
+            else
+            {
+                Console.WriteLine($" The Value {read} isn´t found");
+            }
+            break;
+
+        case "5":
+            Console.WriteLine("Enter value to remove: ");
+            var valueToRemove = Console.ReadLine();
+            if (valueToRemove == null)
+            {
+                Console.WriteLine("Invalid value");
+                break;
+            }
+            else if (!list.Contains(valueToRemove))
+            {
+                Console.WriteLine("Value not found in the list");
+                break;
+            }
+
+            list.Remove(valueToRemove!);
+            Console.WriteLine($"Value {valueToRemove} removed from the list");
+            break;
+
+        case "6":
+            list.Reverse();
+            Console.WriteLine("List reversed");
+            list.PrintList();
+            break;
+
         default:
             Console.WriteLine("Invalid option");
             break;
     }
-    } while (opc != "0");
+} while (opc != "0");
 
 string Menu()
 {
     Console.WriteLine();
-    Console.WriteLine();
     Console.WriteLine("1. Insert at Beginning");
     Console.WriteLine("2. Insert at End");
     Console.WriteLine("3. Print List");
+    Console.WriteLine("4. Search for Value");
+    Console.WriteLine("5. Remove Value");
+    Console.WriteLine("6. Reverse List");
     Console.WriteLine("0. Exit");
+    Console.WriteLine();
     Console.Write("Choose an option: ");
+    Console.WriteLine();
     return Console.ReadLine()!;
 }
--- LinkedList/LinkedList.Core/SimplyLinkedList.cs
+++ LinkedList/LinkedList.Core/SimplyLinkedList.cs
@@ -8,29 +8,29 @@ namespace LinkedList.Core;
 
 public class SimplyLinkedList<T>
 {
-    private SimpleNode<T>? head;
+    private SimpleNode<T>? _head;
 
     public SimplyLinkedList()
     {
-        head = null;
+        _head = null;
     }
 
     public void InsrtAtBeginning(T data)
     {
         var newNode = new SimpleNode<T>(data);
-        newNode.Next = head;
-        head = newNode;
+        newNode.Next = _head;
+        _head = newNode;
     }
 
     public void InsrtAtEnd(T data)
     {
         var newNode = new SimpleNode<T>(data);
-        if (head == null)
+        if (_head == null)
         {
-            head = newNode;
+            _head = newNode;
             return;
         }
-        var current = head;
+        var current = _head;
         while (current.Next != null)
         {
             current = current.Next;
@@ -38,9 +38,61 @@ public class SimplyLinkedList<T>
         current.Next = newNode;
     }
 
+    public bool Contains(T data)
+    {
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data!.Equals(data))
+            {
+                return true;
+            }
+            current = current.Next;
+        }
+        return false;
+    }
+
+    public void Remove(T data)
+    {
+        if (_head == null)
+        {
+            return;
+        }
+        if (_head.Data!.Equals(data))
+        {
+            _head = _head.Next;
+            return;
+        }
+
+        SimpleNode<T> current = _head;
+        while (current.Next != null && !current.Next.Data!.Equals(data))
+        {
+            current = current.Next;
+        }
+        if (current.Next != null)
+        {
+            current.Next = current.Next.Next;
+        }
+    }
+
+    public void Reverse()
+    {
+        SimpleNode<T>? prev = null;
+        SimpleNode<T> current = _head!;
+        SimpleNode<T>? next = null;
+        while (current != null)
+        {
+            next = current.Next;
+            current.Next = prev;
+            prev = current;
+            current = next!;
+        }
+        _head = prev;
+    }
+
     public void PrintList()
     {
-        var current = head;
+        var current = _head;
         while (current != null)
         {
             Console.Write($"{current.Data} -> ");
